2d8561bae30eee83b18f8864a4a960d9c0a6eb5b
Marco Neves <marconunoneves@gmail.com>
2024-01-05 15:33:38 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,39 +1,13 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
 
  module.exports = app;
